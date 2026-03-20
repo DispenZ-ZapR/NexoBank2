bdeebb6da71934f9e7cdb5fdb0c6c5df21297c40
Эмир <aitbemr@gmail.com>
2026-03-20 23:35:38 +0600

    commit

---
+++ src/main/java/com/example/nexobank2/NexoBank2Application.java
@@ -0,0 +1,13 @@
+package com.example.nexobank2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoBank2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexoBank2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nexobank2/NexoBank2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexobank2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoBank2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
