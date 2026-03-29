caad3bc4278cadcf46c2d5d048b4bc7aedd7a260
Эмир <aitbemr@gmail.com>
2026-03-29 16:00:56 +0600

    - для ответа добавил данные паспорта

--- src/main/java/com/example/nexobank2/dto/UserResponse.java
+++ src/main/java/com/example/nexobank2/dto/UserResponse.java
@@ -5,15 +5,18 @@ import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Getter
 @Setter
 public class UserResponse {
     private Long id;
     private String email;
-    private String userType;
     private String phoneNumber;
+    private String userType;
     private LocalDateTime createdAt;
-    private boolean isActive;
     private LocalDateTime deletedAt;
+    private String firstName;
+    private String lastName;
+    private String middleName;
 }
