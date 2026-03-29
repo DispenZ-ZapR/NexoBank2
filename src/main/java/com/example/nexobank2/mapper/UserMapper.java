ae8861a7404e40b4da7aec37c0d32773cfab7843
Эмир <aitbemr@gmail.com>
2026-03-29 16:16:00 +0600

    - изменил маппер, добавил маппинг данных паспорта

--- src/main/java/com/example/nexobank2/mapper/UserMapper.java
+++ src/main/java/com/example/nexobank2/mapper/UserMapper.java
@@ -3,27 +3,26 @@ package com.example.nexobank2.mapper;
 import com.example.nexobank2.dto.UserRequest;
 import com.example.nexobank2.dto.UserResponse;
 import com.example.nexobank2.entity.User;
-import com.example.nexobank2.service.PassportService;
+import com.example.nexobank2.service.impl.PassportService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
+import java.util.List;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = PassportService.class)
 public interface UserMapper {
-    @Mapping(source = "password", target = "passwordHash")
-    @Mapping(target = "passport", source = "passportId", qualifiedByName = "getById")
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "isActive", ignore = true)
-    @Mapping(target = "deletedAt", ignore = true)
-    @Mapping(target = "userType",source = "userType")
+    @Mapping(source = "firstName", target = "passport.firstName")
+    @Mapping(source = "lastName", target = "passport.lastName")
+    @Mapping(source = "middleName", target = "passport.middleName")
+    @Mapping(source = "dateOfBirth", target = "passport.dateOfBirth")
+    @Mapping(source = "personalNumber",target = "passport.personalNumber")
+    @Mapping(source = "passportNumber",target = "passport.passportNumber")
+    @Mapping(source = "userType", target = "userType")
     User toEntity(UserRequest userRequest);
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "email", source = "email")
-    @Mapping(target = "userType", source = "userType")
-    @Mapping(target = "phoneNumber", source = "phoneNumber")
-    @Mapping(target = "createdAt", source = "createdAt")
-    @Mapping(target = "active", source = "isActive")
-    @Mapping(target = "deletedAt", source = "deletedAt")
+    @Mapping(source = "passport.firstName", target = "firstName")
+    @Mapping(source = "passport.lastName", target = "lastName")
+    @Mapping(source = "passport.middleName", target = "middleName")
     UserResponse toResponse(User user);
+    List<UserResponse> toUserResponseList(List<User> users);
 }
