831a9d02f1880cefc5388a6915cf0d4c91b88f69
Эмир <aitbemr@gmail.com>
2026-03-29 16:00:05 +0600

    - Сделал так что бы в запрос входил еще данные для паспорта

--- src/main/java/com/example/nexobank2/dto/UserRequest.java
+++ src/main/java/com/example/nexobank2/dto/UserRequest.java
@@ -1,14 +1,22 @@
 package com.example.nexobank2.dto;
 
+import com.example.nexobank2.enums.UserType;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class UserRequest {
-    private Long passportId;
     private String email;
-    private String password;
     private String phoneNumber;
-    private String userType;
+    private UserType userType;
+    // данные паспорта
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private LocalDate dateOfBirth;
+    private String personalNumber;
+    private String passportNumber;
 }
