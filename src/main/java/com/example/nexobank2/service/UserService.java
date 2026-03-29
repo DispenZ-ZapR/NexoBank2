5860eeb0a77cbc96793874a48cdbf05cd5503b5b
Эмир <aitbemr@gmail.com>
2026-03-29 16:16:23 +0600

    - интерфейс методов

---
+++ src/main/java/com/example/nexobank2/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.nexobank2.service;
+
+import com.example.nexobank2.entity.User;
+import com.example.nexobank2.enums.UserType;
+
+import java.util.List;
+
+public interface UserService extends BaseService<User>{
+    User findByEmail(String email);
+    List<User> findByUsersType(UserType UserType);
+    User findByPassportId(Long passportId);
+    User findByPhoneNumber(String phoneNumber);
+    void changeEmail(Long userId,String email);
+    void changePhoneNumber(Long userId,String phoneNumber);
+    void activateAccount(String password, String token);
+}
