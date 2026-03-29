ba63a2ddb94192ed4d66cdb4661d47f07e7f2204
Эмир <aitbemr@gmail.com>
2026-03-29 16:08:33 +0600

    - Добавлена конфигурация OpenApi

---
+++ src/main/java/com/example/nexobank2/config/OpenApiConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.nexobank2.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(
+        title = "NexoBank API",
+        version = "1.0",
+        description = "API documentation for NexoBank"
+))
+public class OpenApiConfiguration {
+}
