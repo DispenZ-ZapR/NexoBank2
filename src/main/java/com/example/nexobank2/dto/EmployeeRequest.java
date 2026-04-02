589b1a3602f67f0b3f4afcf08be46e5541c7fcf3
Эмир <aitbemr@gmail.com>
2026-04-03 00:22:48 +0600

    - была добавлена данные user и паспорта

--- src/main/java/com/example/nexobank2/dto/EmployeeRequest.java
+++ src/main/java/com/example/nexobank2/dto/EmployeeRequest.java
@@ -1,13 +1,28 @@
 package com.example.nexobank2.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeRequest {
+    private String email;
+    private String phoneNumber;
+
+    // данные для passport
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private LocalDate dateOfBirth;
+    private String personalNumber;
+    private String passportNumber;
+
+    // данные специфичные для employee
     private Long positionId;
     private BigDecimal salary;
-    private Long userId;
 }
