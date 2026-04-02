70cd9800aa8242a3c2c32f1dffa4baa432207085
Эмир <aitbemr@gmail.com>
2026-04-03 00:24:57 +0600

    - реализован интерфейс EmployeePositionService

--- src/main/java/com/example/nexobank2/service/impl/PositionService.java
+++ src/main/java/com/example/nexobank2/service/impl/PositionService.java
@@ -1,17 +1,38 @@
 package com.example.nexobank2.service.impl;
 
+import com.example.nexobank2.entity.BaseEntity;
 import com.example.nexobank2.entity.EmployeePosition;
 import com.example.nexobank2.repository.EmployeePositionRepository;
+import com.example.nexobank2.service.EmployeePositionService;
 import lombok.AllArgsConstructor;
 import org.mapstruct.Named;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
-public class PositionService {
-    private final EmployeePositionRepository employeePositionRepository;
+public class PositionService implements EmployeePositionService  {
+    private final EmployeePositionRepository repository;
+
+    @Override
+    public EmployeePosition save(EmployeePosition entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
     @Named("findByIdP")
-    public EmployeePosition findByIdP(Long id){
-        return employeePositionRepository.findById(id).orElseThrow(()-> new RuntimeException("not found"));
+    public EmployeePosition findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Position not found"));
+    }
+
+    @Override
+    public List<EmployeePosition> findAll() {
+        return repository.findAll();
     }
 }
