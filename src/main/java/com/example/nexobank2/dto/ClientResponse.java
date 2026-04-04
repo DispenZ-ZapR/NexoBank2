21a81ba88f350c2e878c93dbdb70da25952db8cf
Эмир <aitbemr@gmail.com>
2026-04-05 00:06:03 +0600

    - упорядочены и добавлены поля юзера и паспорта

--- src/main/java/com/example/nexobank2/dto/ClientResponse.java
+++ src/main/java/com/example/nexobank2/dto/ClientResponse.java
@@ -1,19 +1,39 @@
 package com.example.nexobank2.dto;
 
 import com.example.nexobank2.enums.ClientStatus;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@JsonPropertyOrder({
+        "id",
+        "firstName" ,
+        "lastName" ,
+        "middleName" ,
+        "email" ,
+        "phoneNumber" ,
+        "creditRating" ,
+        "dateOfBirth" ,
+        "createdAt" ,
+        "deletedAt"
+})
 public class ClientResponse {
     private Long id;
-    private Long userId;
+    private String email;
+    private String phoneNumber;
     private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
     private Integer creditRating;
-    private Long accountId;
-    private ClientStatus clientStatus;
+    // паспортные данные разворачиваем сюда
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private LocalDate dateOfBirth;
 }
 
