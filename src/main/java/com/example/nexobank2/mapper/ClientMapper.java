cf07fd98b5c902ef0b1798898ef32d1ecebc1f76
Эмир <aitbemr@gmail.com>
2026-04-05 00:05:08 +0600

    - добавлен маппинг паспорта и юзера

--- src/main/java/com/example/nexobank2/mapper/ClientMapper.java
+++ src/main/java/com/example/nexobank2/mapper/ClientMapper.java
@@ -10,17 +10,28 @@ import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {UserServiceImpl.class, AccountService.class})
+@Mapper(componentModel = "spring")
 public interface ClientMapper {
-    @Mapping(target = "user", source = "userId", qualifiedByName = "findById")
-    @Mapping(target = "creditRating",source = "creditRating")
-    @Mapping(target = "clientStatus", source = "clientStatus")
+    @Mapping(target = "user.email", source = "email")
+    @Mapping(target = "user.phoneNumber", source = "phoneNumber")
+    @Mapping(target = "clientStatus", constant = "UNVERIFIED")
+    @Mapping(target = "creditRating", constant = "0")
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "user.passport.firstName", source = "firstName")
+    @Mapping(target = "user.passport.lastName", source = "lastName")
+    @Mapping(target = "user.passport.middleName", source = "middleName")
+    @Mapping(target = "user.passport.dateOfBirth", source = "dateOfBirth")
+    @Mapping(target = "user.passport.personalNumber", source = "personalNumber")
+    @Mapping(target = "user.passport.passportNumber", source = "passportNumber")
     Client toEntity(ClientRequest request);
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "userId",source = "user.id")
-    @Mapping(target = "createdAt", source = "createdAt")
-    @Mapping(target = "creditRating", source = "creditRating")
-    @Mapping(target = "clientStatus", source = "clientStatus")
+    @Mapping(source = "user.passport.firstName", target = "firstName")
+    @Mapping(source = "user.passport.lastName", target = "lastName")
+    @Mapping(source = "user.passport.middleName", target = "middleName")
+    @Mapping(source = "user.passport.dateOfBirth", target = "dateOfBirth")
+    @Mapping(source = "user.email", target = "email")
+    @Mapping(source = "user.phoneNumber", target = "phoneNumber")
+    @Mapping(source = "user.createdAt", target = "createdAt")
+    @Mapping(source = "user.deletedAt", target = "deletedAt")
     ClientResponse toResponse(Client client);
 
     List<ClientResponse> toResponseList(List<Client> clients);
