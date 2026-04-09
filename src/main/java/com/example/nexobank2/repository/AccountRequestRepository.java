efd6d2f19ac49f33ecf1e095b7efa794ba38d98b
Эмир <aitbemr@gmail.com>
2026-04-09 19:14:33 +0600

    - репозиторий заявок на счет

---
+++ src/main/java/com/example/nexobank2/repository/AccountRequestRepository.java
@@ -0,0 +1,20 @@
+package com.example.nexobank2.repository;
+
+import com.example.nexobank2.entity.AccountRequest;
+import com.example.nexobank2.enums.AccountRequestStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccountRequestRepository extends JpaRepository<AccountRequest, Long> {
+    
+    List<AccountRequest> findByStatus(AccountRequestStatus status);
+    
+    List<AccountRequest> findByClientId(Long clientId);
+    
+    List<AccountRequest> findByClientIdAndStatus(Long clientId, AccountRequestStatus status);
+    
+    List<AccountRequest> findByApprovedById(Long employeeId);
+}
