525aa66be5ce6198bd94f727a02aab3852504660
Эмир <aitbemr@gmail.com>
2026-04-12 23:45:51 +0600

    - токен истек

---
+++ src/main/java/com/example/nexobank2/exception/TokenExpiredException.java
@@ -0,0 +1,15 @@
+package com.example.nexobank2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class TokenExpiredException extends BaseException{
+    public TokenExpiredException(String message) {
+        super(message);
+    }
+    
+    public TokenExpiredException() {
+        super("Токен истек");
+    }
+}
