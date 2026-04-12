225a95133b01cfb12c6597f9d6f007f5bf38a9f8
Эмир <aitbemr@gmail.com>
2026-04-12 23:43:58 +0600

    - не достаточно прав

---
+++ src/main/java/com/example/nexobank2/exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package com.example.nexobank2.exception;
+
+public class ForbiddenException extends BaseException{
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
