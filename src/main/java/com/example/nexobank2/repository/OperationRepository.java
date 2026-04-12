834b42991b9c0d1c0974ffdf430b7011e24ebe3a
Эмир <aitbemr@gmail.com>
2026-04-12 22:18:19 +0600

    - sql запросы

--- src/main/java/com/example/nexobank2/repository/OperationRepository.java
+++ src/main/java/com/example/nexobank2/repository/OperationRepository.java
@@ -1,12 +1,16 @@
 package com.example.nexobank2.repository;
 
 import com.example.nexobank2.entity.Operation;
+import com.example.nexobank2.enums.OperationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OperationRepository extends JpaRepository<Operation, UUID> {
+    Optional<List<Operation>> findByInitiatorId(Long id);
+    List<Operation> findByStatus(OperationStatus status);
 }
