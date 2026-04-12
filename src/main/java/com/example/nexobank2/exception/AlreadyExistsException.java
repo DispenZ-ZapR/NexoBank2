231ea3b6c1b7b750c29bd47b3027337ef3311fc0
Эмир <aitbemr@gmail.com>
2026-04-12 23:43:05 +0600

    - Существование ресурса

---
+++ src/main/java/com/example/nexobank2/exception/AlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.example.nexobank2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AlreadyExistsException extends BaseException{
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
