7b51965b7850b23a254aae0518dbec442a1c6cd5
Эмир <aitbemr@gmail.com>
2026-04-12 22:18:04 +0600

    - полностью изменен маппер

--- src/main/java/com/example/nexobank2/mapper/OperationMapper.java
+++ src/main/java/com/example/nexobank2/mapper/OperationMapper.java
@@ -3,12 +3,22 @@ package com.example.nexobank2.mapper;
 import com.example.nexobank2.dto.OperationRequest;
 import com.example.nexobank2.dto.OperationResponse;
 import com.example.nexobank2.entity.Operation;
+import com.example.nexobank2.entity.Transaction;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface OperationMapper {
-    Operation toEntity(OperationRequest operationRequest);
-    @Mapping(target = "initiatorId", source = "initiator.id")
-    OperationResponse toResponse(Operation operation);
+    @Mapping(source = "operation.initiator.id", target = "initiatorId")
+    @Mapping(source = "operation.status", target = "status")
+    @Mapping(source = "operation.reason", target = "reason")
+    @Mapping(source = "operation.channel", target = "channel")
+    @Mapping(source = "operation.createdAt", target = "createdAt")
+    @Mapping(source = "transactions", target = "transactions")
+    OperationResponse toResponse(Operation operation, List<Transaction> transactions);
+
+    List<OperationResponse> toResponseList(List<Operation> operations);
+
 }
