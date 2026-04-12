10199a6ee6520d33873d5088ae0ae331073ffa4e
Эмир <aitbemr@gmail.com>
2026-04-12 22:19:41 +0600

    - добавлено поле List transactions

--- src/main/java/com/example/nexobank2/dto/OperationResponse.java
+++ src/main/java/com/example/nexobank2/dto/OperationResponse.java
@@ -4,15 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class OperationResponse {
     private UUID id;
+    private Long initiatorId;
     private String status;
     private String reason;
     private String channel;
     private LocalDateTime createdAt;
-    private Long initiatorId;
+    private List<TransactionResponse> transactions;
 }
