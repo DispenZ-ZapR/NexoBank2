05278141e3ad04b29c644f8f8942cabe175cae7a
Эмир <aitbemr@gmail.com>
2026-04-12 22:21:52 +0600

    добавлены нереализованные методы

---
+++ src/main/java/com/example/nexobank2/service/OperationService.java
@@ -0,0 +1,19 @@
+package com.example.nexobank2.service;
+
+import com.example.nexobank2.dto.TransactionRequest;
+import com.example.nexobank2.entity.Operation;
+import com.example.nexobank2.entity.Transaction;
+import com.example.nexobank2.entity.User;
+import com.example.nexobank2.enums.OperationStatus;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OperationService {
+    Operation transfer (TransactionRequest request, User initiator);
+    Operation getByUUID (UUID uuid);
+    List<Operation> getByAll();
+    List<Operation> findByInitiatorId(Long id);
+    List<Operation> findByStatus(OperationStatus status);
+
+}
