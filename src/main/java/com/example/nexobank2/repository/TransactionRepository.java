dc37a40f2106ec3bc9f16dbc78b268ce68fcea86
Эмир <aitbemr@gmail.com>
2026-04-18 21:48:03 +0600

    - метод для пойска транзакций через id юзера и счета

--- src/main/java/com/example/nexobank2/repository/TransactionRepository.java
+++ src/main/java/com/example/nexobank2/repository/TransactionRepository.java
@@ -7,5 +7,9 @@ import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query("SELECT t from Transaction t where t.account.client.user.id = :userId")
+    List<Transaction> findByUserId(@Param("userId") Long userId);
+
+    List<Transaction> findByAccountIdOrderByTransactionDateDesc(Long accountId);
     List<Transaction> findByOperationId(UUID operationId);
 }
