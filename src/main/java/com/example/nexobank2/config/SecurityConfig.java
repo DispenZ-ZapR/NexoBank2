a9f139564cce9334dde1b2529f61326c1edd3a76
Эмир <aitbemr@gmail.com>
2026-04-18 23:16:40 +0600

    - исправлена ошибка

--- src/main/java/com/example/nexobank2/config/SecurityConfig.java
+++ src/main/java/com/example/nexobank2/config/SecurityConfig.java
@@ -23,10 +23,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers(
-                        "/scalar/**","/v3/api-docs/**","/api/client/save","/api/employees/save","/api/user/verify/**","/api/auth/**").permitAll().anyRequest().authenticated()
-                        .requestMatchers("api/client/getMyProfile").hasRole("CLIENT")
-                        .requestMatchers("api/client/**","api/employees/**").hasAnyRole("EMPLOYEE","ADMIN"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/scalar/**","/v3/api-docs/**","/api/client/save","/api/employees/save","/api/user/verify/**","/api/auth/**").permitAll()
+                        .requestMatchers("/api/client/getMyProfile").hasRole("CLIENT")
+                        .requestMatchers("/api/client/**","/api/employees/**").hasAnyRole("EMPLOYEE","ADMIN")
+                        .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults());
