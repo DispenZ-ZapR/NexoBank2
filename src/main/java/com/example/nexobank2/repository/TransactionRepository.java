8e6f2e673138567b30756abb0fec06d165423b6a
Эмир <aitbemr@gmail.com>
2026-04-18 21:48:27 +0600

    - импорта

--- src/main/java/com/example/nexobank2/repository/TransactionRepository.java
+++ src/main/java/com/example/nexobank2/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.example.nexobank2.repository;
 
 import com.example.nexobank2.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
