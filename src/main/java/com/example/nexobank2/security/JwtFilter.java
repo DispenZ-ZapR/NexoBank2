5117bf0d6233e829564f2daa4cd280b96bea15d9
Эмир <aitbemr@gmail.com>
2026-04-18 21:28:09 +0600

    - класс JwtFilter для проверки токена при каждом запросе

---
+++ src/main/java/com/example/nexobank2/security/JwtFilter.java
@@ -0,0 +1,50 @@
+package com.example.nexobank2.security;
+
+import com.example.nexobank2.service.impl.JwtService;
+import com.example.nexobank2.service.impl.UserDetailService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserDetailService userDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String token = authHeader.substring(7);
+        if(!jwtService.validateToken(token)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String email = jwtService.extractEmail(token);
+        if (SecurityContextHolder.getContext().getAuthentication() != null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        UserDetails userDetails = userDetailService.loadUserByUsername(email);
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
+        auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        filterChain.doFilter(request,response);
+    }
+}
