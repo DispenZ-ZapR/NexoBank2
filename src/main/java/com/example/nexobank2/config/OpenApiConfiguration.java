769907c6af8ff1ea008b87567e2041b8911c2145
Эмир <aitbemr@gmail.com>
2026-04-18 21:30:23 +0600

    - настройл swagger для работы с security

--- src/main/java/com/example/nexobank2/config/OpenApiConfiguration.java
+++ src/main/java/com/example/nexobank2/config/OpenApiConfiguration.java
@@ -1,12 +1,21 @@
 package com.example.nexobank2.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(info = @Info(
         title = "NexoBank API",
         version = "1.0",
         description = "API documentation for NexoBank"
 ))
+@SecurityRequirement(name = "BearerAuth")
+@SecurityScheme(name = "BearerAuth",
+        description = "JWT token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT")
 public class OpenApiConfiguration {
 }
