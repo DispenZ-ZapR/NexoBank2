0b0d35cd8a71de462f4b8dd5c08475864da45969
Эмир <aitbemr@gmail.com>
2026-04-20 23:41:03 +0600

    - импорты

--- src/main/java/com/example/nexobank2/repository/ClientRepository.java
+++ src/main/java/com/example/nexobank2/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.example.nexobank2.repository;
 
 import com.example.nexobank2.entity.Client;
 import com.example.nexobank2.enums.ClientStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
