f2e7892da0ae5ca30d38cf8e44a0eb738b17b5f8
Эмир <aitbemr@gmail.com>
2026-06-17 03:56:27 +0600

    - сервис для типа счета

---
+++ src/main/java/com/example/nexobank2/service/impl/AccountTypeServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.nexobank2.service.impl;
+
+import com.example.nexobank2.entity.AccountType;
+import com.example.nexobank2.exception.NotFoundException;
+import com.example.nexobank2.repository.AccountTypeRepository;
+import com.example.nexobank2.service.AccountTypeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+public class AccountTypeServiceImpl implements AccountTypeService {
+    private final AccountTypeRepository repository;
+    @Override
+    public AccountType save(AccountType entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public AccountType findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new NotFoundException("Не найдено"));
+    }
+
+    @Override
+    public List<AccountType> findAll(int page, int size) {
+        return repository.findAll();
+    }
+}
