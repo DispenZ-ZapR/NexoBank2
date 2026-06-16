94d2ff1210bea57eb9f4add664403d38aaeb289f
Эмир <aitbemr@gmail.com>
2026-06-17 03:58:19 +0600

    - поле id за маппил на игнор

--- src/main/java/com/example/nexobank2/mapper/EmployeePositionMapper.java
+++ src/main/java/com/example/nexobank2/mapper/EmployeePositionMapper.java
@@ -4,11 +4,13 @@ import com.example.nexobank2.dto.EmployeePositionRequest;
 import com.example.nexobank2.dto.EmployeePositionResponse;
 import com.example.nexobank2.entity.EmployeePosition;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EmployeePositionMapper {
+    @Mapping(target = "id", ignore = true)
     EmployeePosition toEntity(EmployeePositionRequest request);
     EmployeePositionResponse toResponse(EmployeePosition employeePosition);
     List<EmployeePositionResponse> toResponseList(List<EmployeePosition> positions);
